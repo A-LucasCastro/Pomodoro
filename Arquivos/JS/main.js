255c1cba4e6a617e585fe3c3020dbd2a9ade580c
A-LucasCastro <luquinhascastro1@hotmail.com>
2022-12-18 23:38:36 -0300

    Tentando criar a contagem regressiva

--- Arquivos/JS/main.js
+++ Arquivos/JS/main.js
@@ -1,15 +1,30 @@
-const pomodoro = document.querySelector('.pomodoro')
-const intervaloCurto = document.querySelector('.intervaloCurto')
-const intervaloGrande = document.querySelector('.intervaloGrande')
+const pomodoro = document.querySelector(".pomodoro");
+const intervaloCurto = document.querySelector(".intervaloCurto");
+const intervaloGrande = document.querySelector(".intervaloGrande");
 
-const timer = document.querySelector('.timer')
-let minutos = 25
-let segundos = 00
+const timer = document.querySelector(".timer");
+let minutos = 25;
+let segundos = 0;
 
-timer.innerHTML = `${minutos}:${segundos}`
+timer.innerHTML = `${minutos}:${zeroAEsquerda(segundos)}`;
+
+function zeroAEsquerda(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+function contador() {
+    if (minutos >= 0 || segundos >= 0) {
+        if (segundos == 0) {
+            segundos = 59;
+            minutos--;
+        } else {
+            segundos--
+        }
+        timer.innerHTML = `${minutos}:${zeroAEsquerda(segundos)}`;				
+			setTimeout(contador(), 1000);	
+    }
+}
 
 function comecarPomodoro() {
-    setInterval(function() {
-        segundos -= segundos
-    }, 1000)
+  contador()
 }
